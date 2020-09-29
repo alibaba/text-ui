a83a0bd416dcf2a88cf0f44c6e964a88bc430851
gongdewei <kylixs@qq.com>
2020-09-29 18:06:56 +0800

    Improve replace CRLF in label element

--- src/main/java/com/taobao/text/util/Utils.java
+++ src/main/java/com/taobao/text/util/Utils.java
@@ -147,6 +147,33 @@ public class Utils {
     }
   }
 
+  public static String replace(String inString, String oldPattern, String newPattern) {
+    if(hasLength(inString) && hasLength(oldPattern) && newPattern != null) {
+      int pos = 0;
+      int index = inString.indexOf(oldPattern);
+      if (index < 0) {
+        //no need to replace
+        return inString;
+      }
+
+      StringBuilder sb = new StringBuilder();
+      for(int patLen = oldPattern.length(); index >= 0; index = inString.indexOf(oldPattern, pos)) {
+        sb.append(inString.substring(pos, index));
+        sb.append(newPattern);
+        pos = index + patLen;
+      }
+
+      sb.append(inString.substring(pos));
+      return sb.toString();
+    } else {
+      return inString;
+    }
+  }
+
+  public static boolean hasLength(CharSequence str) {
+    return str != null && str.length() > 0;
+  }
+
   private static final Pattern blankPattern = Pattern.compile("^\\s*$");
 
   public static boolean notBlank(String s) {
